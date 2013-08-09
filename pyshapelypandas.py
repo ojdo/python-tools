e34c23d7fabe2d30ed1cf1690ac4c70402cfe063
Johannes Dorfner <johannes.dorfner@tum.de>
2013-08-09 11:13:33 +0200

    finished pyshapelypandas.write_shp/read_shp

--- pyshapelypandas.py
+++ pyshapelypandas.py
@@ -1,10 +1,12 @@
 """ I/O functionality for manipulating shapefiles with DataFrames """
 import itertools
+import numpy as np
 import pandas as pd
 import shapefile
 from shapely.geometry import LineString, Point, Polygon
 
 def read_shp(filename):
+    """Read shapefile to dataframe w/ geometry."""
     sr = shapefile.Reader(filename)
     
     cols = sr.fields[:] # [:] = duplicate field list
@@ -26,4 +28,49 @@ def read_shp(filename):
     
     data = [r+[g] for r,g in itertools.izip(records, geometries)]
     
-    return pd.DataFrame(data, columns=cols)
+    df = pd.DataFrame(data, columns=cols)
+    df = df.convert_objects(convert_numeric=True)
+    return df
+
+def write_shp(dataframe, filename):
+    """Write dataframe w/ geometry to shapefile."""
+    
+    df = dataframe.copy()
+    
+    # split geometry column from dataframe
+    geometry = df.pop('geometry')
+        
+    # write geometries to shp/shx, according to geometry type
+    if isinstance(geometry[0], Point):
+        sw = shapefile.Writer(shapefile.POINT)
+        for point in geometry:
+            sw.point(point.x, point.y)
+        
+    elif isinstance(geometry[0], LineString):
+        sw = shapefile.Writer(shapefile.POLYLINE)
+        for line in geometry:
+            sw.line([list(line.coords)])
+        
+    elif isinstance(geometry[0], Polygon):
+        sw = shapefile.Writer(shapefile.POLYGON)
+        for polygon in geometry:
+            sw.poly([list(polygon.exterior.coords)])
+    else:
+        raise NotImplementedError
+        
+    # add fields for dbf
+    for k, column in enumerate(df.columns):
+        if np.issubdtype(df.dtypes[k], np.number):
+            if np.issubdtype(df.dtypes[k], np.floating):
+                sw.field(column, 'N', decimal=5)
+            else:
+                sw.field(column, 'N', decimal=0)
+        else:
+            sw.field(column)
+        
+    # add records to dbf
+    for record in df.itertuples():
+        sw.record(*record[1:]) # drop first tuple element (=index)
+
+    sw.save(filename)
+    
